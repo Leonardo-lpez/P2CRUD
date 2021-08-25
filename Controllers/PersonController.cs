10d9406bf3e0dab95cd4019b5d9d76c6a12b9bc8
Leonardo-Lpez <ericklpezverdugo@gmail.com>
2021-08-25 00:17:26 -0700

    Primer push

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using P2CRUD.Models;
+
+namespace P2CRUD.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly MyDbContext myDb;
+
+        public PersonController(MyDbContext myDb)
+        {
+            this.myDb = myDb;
+        }
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var person = myDb.Person.ToList();
+            return Json(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Person person)
+        {
+            myDb.Person.Add(person);
+            myDb.SaveChanges();
+            return Ok("Ya we no hay");
+        }
+
+        [HttpPut]
+        public IActionResult Put(Person person)
+        {
+            myDb.Entry(person).State = EntityState.Modified;
+            myDb.SaveChanges();
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Person person)
+        {
+            myDb.Entry(person).State = EntityState.Deleted;
+            myDb.SaveChanges();
+            return Ok();
+
+        }
+    }
+
+}
---
+++ Models/Model.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2CRUD.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
